10cc7966f6c9d06d1ee9a13e994bd5df889f7821
P-bot-0ps <143036404+P-bot-0ps@users.noreply.github.com>
2023-09-06 13:01:42 +0400

    added a JS function

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  (temperatureElement.innerHTML = response.data.main), temp;
+}
+let apiKey = "";
+let apiURL = "";
+axios.get(apiURL).then(displayTemperature);
