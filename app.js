c2d0447c4d6796c99587498d74623cb37fe91d9d
P-bot-0ps <143036404+P-bot-0ps@users.noreply.github.com>
2023-09-07 21:56:59 +0400

    added an API

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   (temperatureElement.innerHTML = response.data.main), temp;
 }
-let apiKey = "";
+let apiKey = "6db6bfa3cf6ed8d7cea0399fd7f5588b";
 let apiURL = "";
 axios.get(apiURL).then(displayTemperature);
