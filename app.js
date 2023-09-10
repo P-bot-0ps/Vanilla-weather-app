70c7055e21f832fe9202ddfa2344d864b6254364
P-bot-0ps <143036404+P-bot-0ps@users.noreply.github.com>
2023-09-10 21:19:37 +0400

    added function to java script

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  (temperatureElement.innerHTML = response.data.main), temp;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.main, temp);
+  cityElement.innerHTML = response.data.name;
 }
-let apiKey = "6db6bfa3cf6ed8d7cea0399fd7f5588b";
-let apiURL = "";
+let apiKey = "79c3354a9040bfa5a9aaf908co79t5d3";
+let apiURL =
+  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
 axios.get(apiURL).then(displayTemperature);
