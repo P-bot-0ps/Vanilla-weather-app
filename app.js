5363b10a643503a7ee3cdc0b25e46373cfd1f3c1
P-bot-0ps <143036404+P-bot-0ps@users.noreply.github.com>
2023-09-22 21:31:29 +0300

    working on displaying city and temperature.

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-function displayTemperature(response) {
-  console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.weather[0].description;
-}
 let apiKey = "79c3354a9040bfa5a9aaf908co79t5d3";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=79c3354a9040bfa5a9aaf908co79t5d3&units=metric`;
+console.log(apiUrl);
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayCity);
