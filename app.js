f1502897d5adb656209c2ccb2f137c34b701f198
P-bot-0ps <143036404+P-bot-0ps@users.noreply.github.com>
2023-09-23 22:50:49 +0300

    added a city and temperature element

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = response.data.temperature;
+  cityElement.innerHTML = response.data.city;
+}
 let apiKey = "79c3354a9040bfa5a9aaf908co79t5d3";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=79c3354a9040bfa5a9aaf908co79t5d3&units=metric`;
 console.log(apiUrl);
 
-axios.get(apiUrl).then(displayCity);
+axios.get(apiUrl).then(displayTemperature);
