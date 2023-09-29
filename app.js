afac7d0b489b6103ba71418e9e480c10e888d3f6
P-bot-0ps <143036404+P-bot-0ps@users.noreply.github.com>
2023-09-29 22:46:54 +0300

    added weather forecast

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-function formatDate() {
-  let date = newDate();
+function formatDate(timestamp) {
+  let date = newDate(timestand);
   let hours = date.getHours();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
+  let(minutes < 10);
+  minutes = `0${minutes}`;
+
   let days = [
     "Sunday",
     "Monday",
@@ -14,6 +21,14 @@ function formatDate() {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function getforecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}
+  &appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -21,15 +36,42 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+  let celsiusTemperature = response.data.temperature.current;
   temperatureElement.innerHTML = response.data.temperature.current;
   cityElement.innerHTML = response.data.city;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = response.data.wind.speed;
   descriptionElement.innerHTML = response.data.condition.description;
-  
+  dateElement.innerHTML = formatDate(response.data.temperature.time * 1000);
 }
 let apiKey = "79c3354a9040bfa5a9aaf908co79t5d3";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=79c3354a9040bfa5a9aaf908co79t5d3&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let celsiusTemperature = null;
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+search(NewYork);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", celsiusTemperature);
